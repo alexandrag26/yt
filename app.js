26b008bef7277ebae2c9d9c4253489011c7ff395
alexandrag26 <alexandracisneros111@gmail.com>
2020-10-15 09:48:53 -1200

    first commit

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
-var canal = Mas();
 
-function Mas (){
-var yt = document.getElementById("buscar").value;
-canal = String(yt);
-return canal;
+var Canal= mas();
+
+
+
+function mas(){
+    var yt = document.getElementById("buscar").value;
+    Canal = String(yt);
+    return Canal;
 }
 
 
 
-function youtube(canal) {
-    var canal = Mas();
-    var nextPageToken = "";
-    var resPorPagina = 1;
-    var paginas = 1;
-    var key = "AIzaSyAoQH23oSCJQReeZbGWCH8f2eMyD1QkMwM";
-
-    var url = "https://www.googleapis.com/youtube/v3/search?key=" + key + "&channelId=" + canal + "&part=snippet,id&order=date&maxResults=" + resPorPagina; 
-    var info_canal = "https://www.googleapis.com/youtube/v3/channels?part=snippett%2CcontentDetails%Cstatistics&id=" + canal + "$key=" + key;
-
-    $("#contenedor").append(url);
+    function youtube (Canal){
+        var Canal = mas();
+        var Token = "";
+        var resultados = 1;
+        var paginas = 1;
+        var key = "AIzaSyDvQqACie-xizTWfucZWii5mL17XntFcTY";
+        var url = "https://www.googleapis.com/youtube/v3/search?key=" + key + "&channelId=" + Canal + "&part=snippet,id&order=date&maxResults=" + resultados;
+        var info_canal = "https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=" + Canal + "&key=" + key;
 
 
-    $.getJSON(inf_canal, function (data){
-        var canal_img = data.items[0]["snippet"].thumbnails.default["url"];
-        var canal_title = data.items[0]["snippet"].title;
-        var canal_description = data.items[0]["snippet"].description;
-        var canal_subs = data.items[0]["statistics"].suscriberCount;        
+        $.getJSON(info_canal, function (data) {
+                var canal_name = data.items[0]["snippet"].title;
+                var canal_description = data.items[0]["snippet"].description;
+                var canal_img = data.items[0]["snippet"].thumbnails.default["url"];
+                var canal_subs = data.items[0]["statistics"].subscriberCount;
 
 
-        $("#canal").append(
-            "<p><lavel>Imagen: </lavel>" + canal_img + "</p>" + "<p><lavel>Nombre del canal: </lavel>" + canal_title + "</p>" + "<p><lavel>Descripcion: </lavel>" + canal_description + "</p>" + "<p><lavel>Suscriptores: </lavel>" + canal_subs + "</p>"
-    
-        );
+                $("#canal").append(
+                    '<h3><label>Informacion del canal:</label></h3>'+'<p><label>Icono del canal:</label></p>'+'<img src="'+ canal_img + '"alt="" class="rounded-circle" width=150 height=150> <br>'+'<br>'+'<p><label>Nombre del canal:</label> '+canal_name+'</p>'+'<p><label>Descripcion del canal:</label> '+canal_description+'</p>'+'<p><label>Subscriptores:</label> '+canal_subs+'</p>'
+                );
 
-    });
+        });
+        $.getJSON(url, function (data) {
+            
 
-    $("#contenedor").append(url);
-    $.getJSON(url, function (data) {
+            for (var k in data.items) {
+                var titleVideo=data.items[k]["snippet"].title;
+                var description_video=data.items[k]["snippet"].description;
+                var urlVideo="https://www.youtube.com/watch?v=" + data.items[k]["id"].videoId;
+                var fechaVideo=data.items[k]["snippet"].publishedAt;
+                var video_img = data.items[0]["snippet"].thumbnails.default["url"];
+                console.log(k, data.items[k]["id"].videoId);
+               
+                $("#video").append(
+                    '<h3><label>Informacion del ultimo video:</label></h3>'+'<p><label>Nombre del Video:</label> '+titleVideo+'</p>'+'<p><label>Descripcion del Video:</label> '+ description_video + '</p>'+'<p><label>Fecha y Hora de Publicacion:</label>'+fechaVideo+'</p>'+'<p><label>Url:</label> '+urlVideo+'</p>'+'<img src="'+ video_img + '"alt="" width=250 height=250> <br>'
+                    );
+              
+            }
+            
+        });
 
-        for (var k in data.items) {
-            var titleVideo = data.items[k]["snippet"].title;
-            var description = data.items[k]["snippet"].description;
-            var urlVideo = "https://www.youtube.com/watch?v=" + data.items[k]["id"].videoId;
-            var fechaVideo = data.items[k]["snippet"].publishedAt;
-                            
-            console.log(k, data.items[k]["id"].videoId);
-
-
-            $("#video").append(
-                "<p><lavel>Nombre del video: </lavel>" + titleVideo + "</p>" + "<p><lavel> Descripcion: </lavel>" + description + "</p>" + "<p><lavel>Direccion Url: </lavel>" + urlVideo + "</p>" + "<p><lavel> Fecha y hora de publicacion: </lavel>" + fechaVideo + "</p>"
-    
-            );
-        }
+    }   
         
-    }
 
-
-)};
